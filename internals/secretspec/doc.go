f6dc10904a0f94c3e2dfbb869e2cac3cf5de2b75
Simon Barendse <simon.barendse@gmail.com>
2019-03-26 14:21:42 +0100

    Add package documentation for the secretspec package

---
+++ internals/secretspec/doc.go
@@ -0,0 +1,4 @@
+// Package secretspec provides functionality to set and clear secrets in
+// environment variables, files and fields in configuration files, based
+// on a specification provided in a secrets.yml file.
+package secretspec

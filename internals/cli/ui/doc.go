62ec3e34a56a30937e2c47313b3183560ed0c7b8
Simon Barendse <simon.barendse@gmail.com>
2019-03-27 15:57:42 +0100

    Add package documentation for ui

---
+++ internals/cli/ui/doc.go
@@ -0,0 +1,5 @@
+// Package ui provides functionality to interact with the user.
+//
+// Information can be supplied to the user on stdout and the user
+// can be prompted for input using stdin/stdout or tty.
+package ui

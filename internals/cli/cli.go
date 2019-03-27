e0647adec4953db394ac7e13e9fd63e5e4d90ab1
Simon Barendse <simon.barendse@gmail.com>
2019-03-27 13:49:59 +0100

    Remove the NewLogger function from the common CLI package

--- internals/cli/cli.go
+++ internals/cli/cli.go
@@ -1,11 +1,5 @@
 package cli
 
-import (
-	"os"
-
-	logging "github.com/op/go-logging"
-)
-
 // Injected Variables
 var (
 	// SentryDSN is the dsn used to report errors.
@@ -14,20 +8,3 @@ var (
 	ServerSentryDSN string
 	ClientSentryDSN string
 )
-
-// NewLogger returns a logger with the given format, module and loglevel.
-func NewLogger(format string, module string, debug bool) *logging.Logger {
-	formatter := logging.MustStringFormatter(format)
-	backend := logging.NewBackendFormatter(logging.NewLogBackend(os.Stdout, "", 0), formatter)
-	logging.SetBackend(backend)
-
-	logger := logging.MustGetLogger(module)
-	if debug {
-		logging.SetLevel(logging.DEBUG, module)
-		logger.Debug("Loglevel set to debug")
-	} else {
-		logging.SetLevel(logging.INFO, module)
-	}
-
-	return logger
-}

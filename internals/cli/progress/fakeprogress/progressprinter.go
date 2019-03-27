d74189d76a929cbce7b7f6c45464402f4b8f349c
Simon Barendse <simon.barendse@gmail.com>
2019-03-27 13:19:47 +0100

    Simplify fakeprogress package documentation

--- internals/cli/progress/fakeprogress/progressprinter.go
+++ internals/cli/progress/fakeprogress/progressprinter.go
@@ -1,7 +1,7 @@
 // +build !production
 
-// Package fakeprogress provides an implementation of the Printer interface of the
-// progress package to be used in tests.
+// Package fakeprogress provides an implementation of the progress.Printer interface
+// to be used in tests.
 package fakeprogress
 
 // Printer is a mock of the Printer interface.

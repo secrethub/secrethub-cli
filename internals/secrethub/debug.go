2066ed12f892a8007ec060f24966aae8f7791e8e
Simon Barendse <simon.barendse@gmail.com>
2019-03-28 09:54:22 +0100

    Add a type for the debug flag

--- internals/secrethub/debug.go
+++ internals/secrethub/debug.go
@@ -2,5 +2,5 @@ package secrethub
 
 // RegisterDebugFlag registers a debug flag.
 func RegisterDebugFlag(r FlagRegisterer) {
-	r.Flag("debug", "Enable debug mode.").Short('D')
+	r.Flag("debug", "Enable debug mode.").Short('D').Bool()
 }

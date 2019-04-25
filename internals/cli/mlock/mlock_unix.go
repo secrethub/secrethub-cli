180e69de40b37b012ba2eb664fe67e3b4ccc804e
Simon Barendse <simon.barendse@gmail.com>
2019-04-25 16:41:05 +0200

    Fix typo priviliges => privileges

--- internals/cli/mlock/mlock_unix.go
+++ internals/cli/mlock/mlock_unix.go
@@ -26,7 +26,7 @@ func lockMemory() error {
 				"Could not prevent memory from being written to swap because mlock() is not implemented on this system.\n"+
 				"This usually means that the mlock syscall is not available.\n"+
 				"This requires root privileges as well as a system that supports mlock.\n"+
-				"Please enable mlock on your system, try again with root priviliges,",
+				"Please enable mlock on your system, try again with root privileges,",
 			err)
 	} else if err == unix.ENOMEM {
 		execFile, _ := os.Executable()
@@ -36,7 +36,7 @@ func lockMemory() error {
 			"%s\n\n"+
 				"Could not prevent memory from being written to swap because mlock() failed with ENOMEM.\n"+
 				"This probably means that the user is not allowed to lock enough memory.\n"+
-				"Either execute as root, or give your user the right priviliges by editing the /etc/security/limits.conf file.\n"+
+				"Either execute as root, or give your user the right privileges by editing the /etc/security/limits.conf file.\n"+
 				"On Linux systems, the secrets executable can be given the correct privileges by executing `sudo setcap 'cap_ipc_lock=+ep' %s`.\n"+
 				"It can also mean that the system is out of memory. If this is the case,\n"+
 				"try freeing up some RAM by closing down some applications and see if the problem persists.\n",
@@ -45,8 +45,8 @@ func lockMemory() error {
 		return errMlock.Code("eperm").Errorf(
 			"%s\n\n"+
 				"Could not prevent memory from being written to swap because your are not privileged for this operation.\n"+
-				"This usually means that you need root priviliges.\n"+
-				"Please try again with root priviliges and see if the problem persists.",
+				"This usually means that you need root privileges.\n"+
+				"Please try again with root privileges and see if the problem persists.",
 			err)
 	} else if err != nil {
 		return errio.Error(err)

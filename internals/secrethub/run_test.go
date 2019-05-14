3b13a983258e47ff176f4eba1772478b2b9c123f
Simon Barendse <simon.barendse@gmail.com>
2019-05-14 11:41:37 +0200

    Add testcases

--- internals/secrethub/run_test.go
+++ internals/secrethub/run_test.go
@@ -9,13 +9,16 @@ import (
 
 func TestNewEnv(t *testing.T) {
 	cases := map[string]struct {
-		template string
+		tpl      map[string]string
 		client   fakeclient.WithDataGetter
 		expected map[string]string
 		err      error
 	}{
-		"yml": {
-			template: "foo: bar\nbaz: ${path/to/secret}",
+		"success": {
+			tpl: map[string]string{
+				"yml": "foo: bar\nbaz: ${path/to/secret}",
+				"env": "foo=bar\nbaz=${path/to/secret}",
+			},
 			client: fakeclient.WithDataGetter{
 				ReturnsVersion: &api.SecretVersion{
 					Data: []byte("foobar"),
@@ -26,28 +29,31 @@ func TestNewEnv(t *testing.T) {
 				"baz": "foobar",
 			},
 		},
-		"env": {
-			template: "foo=bar\nbaz=${path/to/secret}",
-			client: fakeclient.WithDataGetter{
-				ReturnsVersion: &api.SecretVersion{
-					Data: []byte("foobar"),
-				},
+		"= in value": {
+			tpl: map[string]string{
+				"yml": "foo: foo=bar",
+				"env": "foo=foo=bar",
 			},
-			expected: map[string]string{
-				"foo": "bar",
-				"baz": "foobar",
-			},
-		},
-		"env = in value": {
-			template: "foo=foo=bar",
 			expected: map[string]string{
 				"foo": "foo=bar",
 			},
 		},
-		"env double ==": {
-			template: "foo==foobar",
+		"double ==": {
+			tpl: map[string]string{
+				"yml": "foo: =foobar",
+				"env": "foo==foobar",
+			},
 			expected: map[string]string{
 				"foo": "=foobar",
+			},
+		},
+		"inject not closed": {
+			tpl: map[string]string{
+				"yml": "foo: ${path/to/secret",
+				"env": "foo=${path/to/secret",
+			},
+			expected: map[string]string{
+				"foo": "${path/to/secret",
 			},
 		},
 	}
@@ -62,13 +68,18 @@ func TestNewEnv(t *testing.T) {
 				},
 			}
 
-			env, err := NewEnv(tc.template)
-			assert.OK(t, err)
+			for format, tpl := range tc.tpl {
+				t.Run(format, func(t *testing.T) {
+					env, err := NewEnv(tpl)
+					assert.OK(t, err)
 
-			actual, err := env.Env(client)
-			assert.OK(t, err)
+					actual, err := env.Env(client)
+					assert.Equal(t, err, tc.err)
 
-			assert.Equal(t, actual, tc.expected)
+					assert.Equal(t, actual, tc.expected)
+				})
+			}
+
 		})
 	}
 }

07feca0fb6f1a23487ae3b09813a0b1c17fc527b
Simon Barendse <simon.barendse@gmail.com>
2019-06-03 15:47:07 +0200

    Split run template tests in parsing env, yml and combining them

--- internals/secrethub/run_test.go
+++ internals/secrethub/run_test.go
@@ -10,97 +10,150 @@ import (
 	"github.com/secrethub/secrethub-go/internals/assert"
 )
 
-func TestNewEnv(t *testing.T) {
+func TestParseEnv(t *testing.T) {
 	cases := map[string]struct {
-		tpl          map[string]string
-		replacements map[string]string
-		expected     map[string]string
-		err          error
+		raw      string
+		expected map[string]string
+		err      error
 	}{
 		"success": {
-			tpl: map[string]string{
-				"yml": "foo: bar\nbaz: ${path/to/secret}",
-				"env": "foo=bar\nbaz=${path/to/secret}",
-			},
-			replacements: map[string]string{
-				"path/to/secret": "foobar",
-			},
+			raw: "foo=bar\nbaz=${path/to/secret}",
 			expected: map[string]string{
 				"foo": "bar",
-				"baz": "foobar",
+				"baz": "${path/to/secret}",
+			},
+		},
+		"= sign in value": {
+			raw: "foo=foo=bar",
+			expected: map[string]string{
+				"foo": "foo=bar",
+			},
+		},
+		"inject not closed": {
+			raw: "foo=${path/to/secret",
+			err: ErrTemplate(1, tpl.ErrTagNotClosed("}")),
+		},
+		"invalid key": {
+			raw: "FOO\000=bar",
+			err: ErrTemplate(1, validation.ErrInvalidEnvarName("FOO\000")),
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			actual, err := parseEnv(tc.raw)
+
+			expected := map[string]tpl.Template{}
+			for k, v := range tc.expected {
+				template, err := tpl.NewParser().Parse(v)
+				assert.OK(t, err)
+				expected[k] = template
+			}
+
+			assert.Equal(t, actual, envTemplate{vars: expected})
+			assert.Equal(t, err, tc.err)
+		})
+	}
+}
+
+func TestParseYML(t *testing.T) {
+	cases := map[string]struct {
+		raw      string
+		expected map[string]string
+		err      error
+	}{
+		"success": {
+			raw: "foo: bar\nbaz: ${path/to/secret}",
+			expected: map[string]string{
+				"foo": "bar",
+				"baz": "${path/to/secret}",
 			},
 		},
 		"= in value": {
-			tpl: map[string]string{
-				"yml": "foo: foo=bar\nbar: baz",
-				"env": "foo=foo=bar\nbar=baz",
-			},
+			raw: "foo: foo=bar\nbar: baz",
 			expected: map[string]string{
 				"foo": "foo=bar",
 				"bar": "baz",
 			},
 		},
-		"double ==": {
-			tpl: map[string]string{
-				"yml": "foo: =foobar\nbar: baz",
-				"env": "foo==foobar\nbar=baz",
-			},
-			expected: map[string]string{
-				"foo": "=foobar",
-				"bar": "baz",
-			},
-		},
-		"inject not closed yml": {
-			tpl: map[string]string{
-				"yml": "foo: ${path/to/secret",
-			},
-			err: ErrTemplate(1, errors.New("template is not formatted as key=value pairs")),
-		},
-		"inject not closed env": {
-			tpl: map[string]string{
-				"env": "foo=${path/to/secret",
-			},
-			err: ErrTemplate(1, tpl.ErrTagNotClosed("}")),
+		"inject not closed": {
+			raw: "foo: ${path/to/secret",
+			err: tpl.ErrTagNotClosed("}"),
 		},
 		"nested yml": {
-			tpl: map[string]string{
-				"yml": `ROOT:
-	SUB:
-		NAME1: val1`,
-			},
-			err: ErrTemplate(1, errors.New("template is not formatted as key=value pairs")),
+			raw: "ROOT:\n\tSUB\n\t\tNAME: val1",
+			err: errors.New("yaml: line 2: found character that cannot start any token"),
 		},
 		"invalid key yml": {
-			tpl: map[string]string{
-				"yml": "FOO\000: bar",
-			},
-			err: ErrTemplate(1, errors.New("template is not formatted as key=value pairs")),
-		},
-		"invalid key env": {
-			tpl: map[string]string{
-				"env": "FOO\000=bar",
-			},
-			err: ErrTemplate(1, validation.ErrInvalidEnvarName("FOO\000")),
+			raw: "FOO=: bar",
+			err: validation.ErrInvalidEnvarName("FOO="),
 		},
 	}
 
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
+			actual, err := parseYML(tc.raw)
 
-			for format, tpl := range tc.tpl {
-				t.Run(format, func(t *testing.T) {
-					env, err := NewEnv(tpl)
-					if err != nil {
-						assert.Equal(t, err, tc.err)
-					} else {
-						actual, err := env.Env(tc.replacements)
-						assert.Equal(t, err, tc.err)
-
-						assert.Equal(t, actual, tc.expected)
-					}
-				})
+			expected := map[string]tpl.Template{}
+			for k, v := range tc.expected {
+				template, err := tpl.NewParser().Parse(v)
+				assert.OK(t, err)
+				expected[k] = template
 			}
 
+			assert.Equal(t, actual, envTemplate{vars: expected})
+			assert.Equal(t, err, tc.err)
+		})
+	}
+}
+
+func TestNewEnv(t *testing.T) {
+	cases := map[string]struct {
+		raw          string
+		replacements map[string]string
+		expected     map[string]string
+		err          error
+	}{
+		"success": {
+			raw: "foo=bar\nbaz=${path/to/secret}",
+			replacements: map[string]string{
+				"path/to/secret": "val",
+			},
+			expected: map[string]string{
+				"foo": "bar",
+				"baz": "val",
+			},
+		},
+		"success yml": {
+			raw: "foo: bar\nbaz: ${path/to/secret}",
+			replacements: map[string]string{
+				"path/to/secret": "val",
+			},
+			expected: map[string]string{
+				"foo": "bar",
+				"baz": "val",
+			},
+		},
+		"yml error": {
+			raw: "foo: ${path/to/secret",
+			err: ErrTemplate(1, errors.New("template is not formatted as key=value pairs")),
+		},
+		"env error": {
+			raw: "foo=${path/to/secret",
+			err: ErrTemplate(1, tpl.ErrTagNotClosed("}")),
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			env, err := NewEnv(tc.raw)
+			if err != nil {
+				assert.Equal(t, err, tc.err)
+			} else {
+				actual, err := env.Env(tc.replacements)
+				assert.Equal(t, err, tc.err)
+
+				assert.Equal(t, actual, tc.expected)
+			}
 		})
 	}
 }

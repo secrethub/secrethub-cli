f522ffc868296047f04f296ec6cc790f601e55f3
Simon Barendse <simon.barendse@gmail.com>
2019-06-21 15:44:36 +0200

    Refactor IsToken function to use an array instead of a map
    An array is more intuitive than a map of structs.
    
    Futhermore, a key lookup on a map of 4 runes is not faster than
    iterating over an array.

--- internals/secrethub/tpl/internal/token/token.go
+++ internals/secrethub/tpl/internal/token/token.go
@@ -7,16 +7,15 @@ var (
 	RBracket  = '}'
 	Backslash = '\\'
 
-	tokens = map[rune]struct{}{
-		Dollar:    {},
-		LBracket:  {},
-		RBracket:  {},
-		Backslash: {},
-	}
+	tokens = []rune{Dollar, LBracket, RBracket, Backslash}
 )
 
 // IsToken returns whether the given rune is a token.
 func IsToken(ch rune) bool {
-	_, isToken := tokens[ch]
-	return isToken
+	for _, token := range tokens {
+		if ch == token {
+			return true
+		}
+	}
+	return false
 }

a070bc0fc3877d3294145fcfc87458fffd4676a3
Simon Barendse <simon.barendse@gmail.com>
2019-06-24 14:40:22 +0200

    Remove now unused error
    This error used to be returned when we now parse a template
    variable without brackets

--- internals/secrethub/tpl/errors.go
+++ internals/secrethub/tpl/errors.go
@@ -21,16 +21,6 @@ func (err templateSyntaxError) Error() string {
 	return tplError.Code(err.code).Errorf("template syntax error at %d:%d: %s", err.lineNo, err.colNo, err.msg).Error()
 }
 
-// ErrUnexpectedDollar is returned when an unescaped dollar sign is followed by a letter or underscore.
-func ErrUnexpectedDollar(lineNo, colNo int) error {
-	return templateSyntaxError{
-		lineNo: lineNo,
-		colNo:  colNo,
-		code:   "unexpected character",
-		msg:    "unexpected '$'. Use '\\$' if you want to output a dollar sign.",
-	}
-}
-
 // ErrUnexpectedCharacter is returned when expecting a specific character, for example
 // the first character of a closing delimiter after a space occurred in a tag, or
 // the second character of a closing delimiter after the first character of the closing

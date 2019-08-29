8624914710c042443736d0e88064656045e8fbe8
Simon Barendse <simon.barendse@gmail.com>
2019-08-29 11:59:41 +0200

    Rephrase secrethub service aws init --region help text

--- internals/secrethub/service_aws_init.go
+++ internals/secrethub/service_aws_init.go
@@ -148,7 +148,7 @@ func (cmd *ServiceAWSInitCommand) Register(r Registerer) {
 	clause.Arg("repo", "The service account is attached to the repository in this path.").Required().SetValue(&cmd.repo)
 	clause.Flag("kms-key-id", "ID of the KMS-key to be used for encrypting the service's account key.").StringVar(&cmd.kmsKeyID)
 	clause.Flag("role", "ARN of the IAM role that should have access to this service account.").StringVar(&cmd.role)
-	clause.Flag("region", "The AWS region that should be used").StringVar(&cmd.region)
+	clause.Flag("region", "The AWS region that should be used for KMS").StringVar(&cmd.region)
 	clause.Flag("description", "A description for the service so others will recognize it. Defaults to the name of the role that is attached to the service.").StringVar(&cmd.description)
 	clause.Flag("descr", "").Hidden().StringVar(&cmd.description)
 	clause.Flag("desc", "").Hidden().StringVar(&cmd.description)

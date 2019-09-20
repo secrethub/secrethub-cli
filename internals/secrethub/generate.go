186a64a764c3c1fabe18dbc629df3fe8374d66ac
Simon Barendse <simon.barendse@gmail.com>
2019-09-20 14:08:19 +0200

    Rename generate rand to generate and make length a flag
    New: generate [--length=<length>] <path>
    Old: generate rand <path> [<length>] still works.

--- internals/secrethub/generate.go
+++ internals/secrethub/generate.go
@@ -2,9 +2,12 @@ package secrethub
 
 import (
 	"fmt"
+	"strconv"
+
+	"github.com/secrethub/secrethub-go/internals/api"
 
 	"github.com/secrethub/secrethub-cli/internals/cli/ui"
-	"github.com/secrethub/secrethub-go/internals/api"
+
 	"github.com/secrethub/secrethub-go/internals/errio"
 	"github.com/secrethub/secrethub-go/pkg/randchar"
 )
@@ -13,16 +16,21 @@ var (
 	errGenerate = errio.Namespace("generate")
 
 	// ErrInvalidRandLength is returned when an invalid length is given.
-	ErrInvalidRandLength = errGenerate.Code("invalid_rand_length").Error("The secret length must be larger than 0")
+	ErrInvalidRandLength         = errGenerate.Code("invalid_rand_length").Error("The secret length must be larger than 0")
+	ErrCannotUseLengthArgAndFlag = errGenerate.Code("length_arg_and_flag").Error("length cannot be provided as an argument and a flag at the same time")
 )
+
+const defaultLength = 22
 
 // GenerateSecretCommand generates a new secret and writes to the output path.
 type GenerateSecretCommand struct {
 	useSymbols bool
 	generator  randchar.Generator
 	io         ui.IO
-	length     int
-	path       api.SecretPath
+	lengthFlag *int
+	firstArg   string
+	secondArg  string
+	lengthArg  *int
 	newClient  newClientFunc
 }
 
@@ -36,12 +44,14 @@ func NewGenerateSecretCommand(io ui.IO, newClient newClientFunc) *GenerateSecret
 
 // Register registers the command, arguments and flags on the provided Registerer.
 func (cmd *GenerateSecretCommand) Register(r Registerer) {
-	generateCommand := r.Command("generate", "").Hidden()
-
-	clause := generateCommand.Command("rand", "Generate a random secret. By default, it uses numbers (0-9), lowercase letters (a-z) and uppercase letters (A-Z) and a length of 22.")
-	clause.Arg("secret-path", "The path to write the generated secret to (<namespace>/<repo>[/<dir>]/<secret>)").Required().SetValue(&cmd.path)
-	clause.Arg("length", "The length of the generated secret. Defaults to 22.").Default("22").IntVar(&cmd.length)
+	clause := r.Command("generate", "Generate a random secret.")
+	clause.HelpLong("By default, it uses numbers (0-9), lowercase letters (a-z) and uppercase letters (A-Z) and a length of 22.")
+	clause.Arg("secret-path", "The path to write the generated secret to (<namespace>/<repo>[/<dir>]/<secret>)").Required().StringVar(&cmd.firstArg)
+	cmd.lengthFlag = clause.Flag("length", "The length of the generated secret. Defaults to "+strconv.Itoa(defaultLength)).PlaceHolder(strconv.Itoa(defaultLength)).Int()
 	clause.Flag("symbols", "Include symbols in secret.").Short('s').BoolVar(&cmd.useSymbols)
+
+	clause.Arg("rand-command", "").Hidden().StringVar(&cmd.secondArg)
+	cmd.lengthArg = clause.Arg("length", "").Hidden().Int()
 
 	// TODO SHDEV-528: implement --clip
 	// clause.Flag("clip", "Copy the secret value to the clipboard. The clipboard is automatically cleared after 45 seconds.").Short('c').BoolVar(cmd.clip)
@@ -62,13 +72,23 @@ func (cmd *GenerateSecretCommand) Run() error {
 
 // run generates a new secret and writes to the output path.
 func (cmd *GenerateSecretCommand) run() error {
-	if cmd.length <= 0 {
+	length, err := cmd.length()
+	if err != nil {
+		return err
+	}
+
+	path, err := cmd.path()
+	if err != nil {
+		return err
+	}
+
+	if length <= 0 {
 		return ErrInvalidRandLength
 	}
 
 	fmt.Fprint(cmd.io.Stdout(), "Generating secret value...\n")
 
-	data, err := cmd.generator.Generate(cmd.length)
+	data, err := cmd.generator.Generate(length)
 	if err != nil {
 		return err
 	}
@@ -80,12 +100,32 @@ func (cmd *GenerateSecretCommand) run() error {
 
 	fmt.Fprint(cmd.io.Stdout(), "Writing secret value...\n")
 
-	version, err := client.Secrets().Write(cmd.path.Value(), data)
+	version, err := client.Secrets().Write(path, data)
 	if err != nil {
 		return err
 	}
 
-	fmt.Fprintf(cmd.io.Stdout(), "Write complete! A randomly generated secret has been written to %s:%d.\n", cmd.path, version.Version)
+	fmt.Fprintf(cmd.io.Stdout(), "Write complete! A randomly generated secret has been written to %s:%d.\n", path, version.Version)
 
 	return nil
+}
+
+func (cmd *GenerateSecretCommand) length() (int, error) {
+	if cmd.lengthArg != nil && cmd.lengthFlag != nil {
+		return 0, ErrCannotUseLengthArgAndFlag
+	}
+	if cmd.lengthFlag != nil {
+		return *cmd.lengthFlag, nil
+	}
+	if cmd.lengthArg != nil {
+		return *cmd.lengthArg, nil
+	}
+	return defaultLength, nil
+}
+
+func (cmd *GenerateSecretCommand) path() (string, error) {
+	if cmd.firstArg == "rand" {
+		return cmd.secondArg, api.ValidateSecretPath(cmd.secondArg)
+	}
+	return cmd.firstArg, api.ValidateSecretPath(cmd.firstArg)
 }

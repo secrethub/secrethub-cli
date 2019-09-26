c9a8549eb507c80e2f880a1f061b33cdb17fd5af
Joris Coenen <jpcoenen@users.noreply.github.com>
2019-09-26 15:59:35 +0200

    Remove references to undefined variables

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
 if [ -d /etc/bash_completion.d/ ]; then
-    echo -e "${OK_COLOR}==> Installing completion for Bash${NO_COLOR}"
+    echo -e "==> Installing completion for Bash"
     /usr/local/bin/secrethub --completion-script-bash > /etc/bash_completion.d/secrethub
 elif [ -d /usr/local/etc/bash_completion.d/ ]; then
-    echo -e "${OK_COLOR}==> Installing completion for Bash${NO_COLOR}"
+    echo -e "==> Installing completion for Bash"
     /usr/local/bin/secrethub --completion-script-bash > /usr/local/etc/bash_completion.d/secrethub
 fi
 
 if command -v zsh > /dev/null 2>&1; then
-    echo -e "${OK_COLOR}==> Installing completion for ZSH${NO_COLOR}"
+    echo -e "==> Installing completion for ZSH"
     mkdir -p ~/.zsh/completion
     /usr/local/bin/secrethub --completion-script-zsh > ~/.zsh/completion/secrethub
 

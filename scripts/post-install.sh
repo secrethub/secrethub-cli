79969658b2ce68d810413dffa2143ab43df9f90a
Joris Coenen <jpcoenen@users.noreply.github.com>
2019-09-26 17:41:51 +0200

    Don't install zsh completion automatically
    Touching someone's .zshrc is a bit too invasive...

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -5,13 +5,3 @@ if [ -d ${BASH_COMPLETION_DIR} ]; then
     echo -e "==> Installing completion for Bash"
     /usr/bin/secrethub --completion-script-bash > ${BASH_COMPLETION_DIR}/secrethub
 fi
-
-if command -v zsh > /dev/null 2>&1; then
-    echo -e "==> Installing completion for ZSH"
-    mkdir -p ~/.zsh/completion
-    /usr/local/bin/secrethub --completion-script-zsh > ~/.zsh/completion/secrethub
-
-    if ! grep -q "source ~/.zsh/completion/secrethub" ~/.zshrc; then
-        echo "source ~/.zsh/completion/secrethub" >> ~/.zshrc
-    fi
-fi

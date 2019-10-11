ec058ed2ba4b5c3375a72b1cf2320a22d813089f
Joris Coenen <jpcoenen@users.noreply.github.com>
2019-10-11 15:28:41 +0200

    Only execute script on remove and check for empty dir name

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 BASH_COMPLETION_DIR=$(pkg-config --variable=completionsdir bash-completion)
-if [ -d ${BASH_COMPLETION_DIR} ]; then
+if [ -d ${BASH_COMPLETION_DIR} ] && [ ${BASH_COMPLETION_DIR} != "" ]; then
     echo -e "==> Installing completion for Bash"
     /usr/bin/secrethub --completion-script-bash > ${BASH_COMPLETION_DIR}/secrethub
 fi
--- scripts/post-remove.sh
+++ scripts/post-remove.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
-BASH_COMPLETION_DIR=$(pkg-config --variable=completionsdir bash-completion)
-if [ -d ${BASH_COMPLETION_DIR} ]; then
-    rm -f ${BASH_COMPLETION_DIR}/secrethub
+# Only execute if this is an uninstall, not an upgrade.
+if [ $1 -eq 0 ]; then
+  BASH_COMPLETION_DIR=$(pkg-config --variable=completionsdir bash-completion)
+  if [ -d ${BASH_COMPLETION_DIR} ] && [ ${BASH_COMPLETION_DIR} != "" ]; then
+      rm -f ${BASH_COMPLETION_DIR}/secrethub
+  fi
 fi

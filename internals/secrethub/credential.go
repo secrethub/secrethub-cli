5055c316db120d31c00061d199f1bdf576874c0e
Joris Coenen <jpcoenen@users.noreply.github.com>
2019-11-20 17:25:35 +0100

    Add credential management command

---
+++ internals/secrethub/credential.go
@@ -0,0 +1,28 @@
+package secrethub
+
+import (
+	"github.com/secrethub/secrethub-cli/internals/cli/ui"
+	"github.com/secrethub/secrethub-cli/internals/secrethub/command"
+)
+
+// CredentialCommand handles operations on SecretHub credentials.
+type CredentialCommand struct {
+	io              ui.IO
+	clientFactory   ClientFactory
+	credentialStore CredentialConfig
+}
+
+// NewCredentialCommand creates a new CredentialCommand.
+func NewCredentialCommand(io ui.IO, clientFactory ClientFactory, credentialStore CredentialConfig) *CredentialCommand {
+	return &CredentialCommand{
+		io:              io,
+		clientFactory:   clientFactory,
+		credentialStore: credentialStore,
+	}
+}
+
+// Register registers the command and its sub-commands on the provided Registerer.
+func (cmd *CredentialCommand) Register(r command.Registerer) {
+	clause := r.Command("credential", "Manage your credentials")
+	NewCredentialBackupCommand(cmd.io, cmd.clientFactory.NewClient, cmd.credentialStore).Register(clause)
+}

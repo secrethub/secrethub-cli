9f69116bd5e5cd3184edfc70786e2a4121c06570
Marton Soos <marton.soos@secrethub.io>
2020-03-26 16:48:56 +0100

    Add more audit table formatting tests

--- internals/secrethub/audit_test.go
+++ internals/secrethub/audit_test.go
@@ -1,6 +1,7 @@
 package secrethub
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/secrethub/secrethub-go/internals/assert"
@@ -53,12 +54,84 @@ func Test_columnFormatter_columnWidths(t *testing.T) {
 			},
 			expected: []int{27, 26, 25, 20},
 		},
+		"no max width for some all fit": {
+			formatter: columnFormatter{
+				tableWidth: 64,
+				columns: []auditTableColumn{
+					{maxWidth: 15},
+					{},
+					{maxWidth: 15},
+				},
+			},
+			expected: []int{15, 30, 15},
+		},
+		"no max width for some not all fit": {
+			formatter: columnFormatter{
+				tableWidth: 64,
+				columns: []auditTableColumn{
+					{maxWidth: 50},
+					{},
+					{maxWidth: 10},
+				},
+			},
+			expected: []int{25, 25, 10},
+		},
 	}
 
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
 			result := tc.formatter.columnWidths()
 			assert.Equal(t, result, tc.expected)
+		})
+	}
+}
+
+func Test_columnFormatter_formatRow(t *testing.T) {
+	cases := map[string]struct {
+		formatter   columnFormatter
+		row         []string
+		expected    string
+		expectedErr error
+	}{
+		"all cells fit": {
+			formatter: columnFormatter{
+				tableWidth:           102,
+				computedColumnWidths: []int{50, 50},
+				columns:              []auditTableColumn{{}, {}},
+			},
+			row:         []string{"foo", "bar"},
+			expected:    "foo" + strings.Repeat(" ", 47) + "  " + "bar" + strings.Repeat(" ", 47),
+			expectedErr: nil,
+		},
+		"wrapping": {
+			formatter: columnFormatter{
+				tableWidth:           6,
+				computedColumnWidths: []int{2, 2},
+				columns:              []auditTableColumn{{}, {}},
+			},
+			row:         []string{"foo", "bar"},
+			expected:    "fo  ba\no   r ",
+			expectedErr: nil,
+		},
+		"fits exactly": {
+			formatter: columnFormatter{
+				tableWidth:           8,
+				computedColumnWidths: []int{3, 3},
+				columns:              []auditTableColumn{{}, {}},
+			},
+			row:         []string{"foo", "bar"},
+			expected:    "foo  bar",
+			expectedErr: nil,
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			result, err := tc.formatter.formatRow(tc.row)
+			assert.Equal(t, err, tc.expectedErr)
+			if err == nil {
+				assert.Equal(t, result, tc.expected)
+			}
 		})
 	}
 }

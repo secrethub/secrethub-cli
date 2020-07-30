405720f96b95508e43293fecfbd58e874caf2673
Eduard Filip <eduard.filip@secrethub.io>
2020-07-30 13:47:40 +0300

    Generalize clipper content assertion + simplify newClient setup

--- internals/secrethub/read_test.go
+++ internals/secrethub/read_test.go
@@ -25,13 +25,15 @@ func TestReadCommand_Run(t *testing.T) {
 		newClientErr    error
 		secretVersion   api.SecretVersion
 		serviceErr      error
-		expectedContent string
+		expectedClip    []byte
+		expectedFileOut []byte
 		out             string
 		err             error
 	}{
 		"success read": {
 			cmd: ReadCommand{
-				path: "test/repo/secret",
+				path:    "test/repo/secret",
+				clipper: fakeclip.New(),
 			},
 			secretVersion: api.SecretVersion{Data: testSecret},
 			out:           string(testSecret) + "\n",
@@ -43,18 +45,19 @@ func TestReadCommand_Run(t *testing.T) {
 				useClipboard:        true,
 				clearClipboardAfter: 5 * time.Minute,
 			},
-			secretVersion:   api.SecretVersion{Data: testSecret},
-			expectedContent: string(testSecret),
-			out:             "Copied test/repo/secret to clipboard. It will be cleared after 5 minutes.\n",
+			secretVersion: api.SecretVersion{Data: testSecret},
+			expectedClip:  testSecret,
+			out:           "Copied test/repo/secret to clipboard. It will be cleared after 5 minutes.\n",
 		},
 		"success file": {
 			cmd: ReadCommand{
 				path:     "test/repo/secret",
 				outFile:  "secret.txt",
 				fileMode: filemode.New(os.ModePerm),
+				clipper:  fakeclip.New(),
 			},
 			secretVersion:   api.SecretVersion{Data: testSecret},
-			expectedContent: string(testSecret) + "\n",
+			expectedFileOut: []byte(string(testSecret) + "\n"),
 			out:             "",
 		},
 		"fail file": {
@@ -62,18 +65,27 @@ func TestReadCommand_Run(t *testing.T) {
 				path:     "test/repo/secret",
 				outFile:  "/fail/read.txt",
 				fileMode: filemode.New(os.ModeAppend),
+				clipper:  fakeclip.New(),
 			},
 			secretVersion: api.SecretVersion{Data: testSecret},
 			out:           "",
 			err:           ErrCannotWrite("/fail/read.txt", "open /fail/read.txt: no such file or directory"),
 		},
 		"new client error": {
-			newClientErr: testErr,
-			err:          testErr,
+			cmd: ReadCommand{
+				clipper: fakeclip.New(),
+			},
+			secretVersion: api.SecretVersion{Data: testSecret},
+			newClientErr:  testErr,
+			err:           testErr,
 		},
 		"read error": {
-			serviceErr: testErr,
-			err:        testErr,
+			cmd: ReadCommand{
+				clipper: fakeclip.New(),
+			},
+			secretVersion: api.SecretVersion{Data: testSecret},
+			serviceErr:    testErr,
+			err:           testErr,
 		},
 	}
 
@@ -83,40 +95,33 @@ func TestReadCommand_Run(t *testing.T) {
 			testIO := fakeui.NewIO(t)
 			tc.cmd.io = testIO
 
-			if tc.newClientErr != nil {
-				tc.cmd.newClient = func() (secrethub.ClientInterface, error) {
-					return nil, tc.newClientErr
-				}
-			} else {
-				tc.cmd.newClient = func() (secrethub.ClientInterface, error) {
-					return fakeclient.Client{
-						SecretService: &fakeclient.SecretService{
-							VersionService: &fakeclient.SecretVersionService{
-								GetWithDataFunc: func(path string) (*api.SecretVersion, error) {
-									return &tc.secretVersion, tc.serviceErr
-								},
+			tc.cmd.newClient = func() (secrethub.ClientInterface, error) {
+				return fakeclient.Client{
+					SecretService: &fakeclient.SecretService{
+						VersionService: &fakeclient.SecretVersionService{
+							GetWithDataFunc: func(path string) (*api.SecretVersion, error) {
+								return &tc.secretVersion, tc.serviceErr
 							},
 						},
-					}, nil
-				}
+					},
+				}, tc.newClientErr
 			}
 
 			// Run
 			err := tc.cmd.Run()
-			content := ""
+			res := []byte("")
 			if _, err := os.Stat(tc.cmd.outFile); err == nil {
-				res, _ := ioutil.ReadFile(tc.cmd.outFile)
-				content = string(res)
+				res, _ = ioutil.ReadFile(tc.cmd.outFile)
 				os.Remove(tc.cmd.outFile)
-			} else if tc.cmd.useClipboard {
-				res, _ := tc.cmd.clipper.ReadAll()
-				content = string(res)
 			}
+			clip, clipErr := tc.cmd.clipper.ReadAll()
 
 			// Assert
 			assert.Equal(t, err, tc.err)
 			assert.Equal(t, testIO.Out.String(), tc.out)
-			assert.Equal(t, content, tc.expectedContent)
+			assert.OK(t, clipErr)
+			assert.Equal(t, clip, tc.expectedClip)
+			assert.Equal(t, res, tc.expectedFileOut)
 		})
 	}
 }

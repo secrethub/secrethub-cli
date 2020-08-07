7870a58e2d72869b7bdd607f1f0377c565763165
Marton Soos <marton.soos@secrethub.io>
2020-08-07 13:07:12 +0200

    Factor out start repo creation and org creation from signup command

--- internals/secrethub/signup.go
+++ internals/secrethub/signup.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/secrethub/secrethub-go/pkg/secrethub"
+
 	"github.com/secrethub/secrethub-cli/internals/cli/progress"
 	"github.com/secrethub/secrethub-cli/internals/cli/ui"
 	"github.com/secrethub/secrethub-cli/internals/secrethub/command"
@@ -166,20 +168,7 @@ func (cmd *SignUpCommand) Run() error {
 		return err
 	}
 
-	// create a start repository and write a fist secret to it, so that
-	// the user can start by reading their first secret.
-	// This is intended to smoothen onboarding.
-	repoPath := secretpath.Join(cmd.username, "start")
-	_, err = client.Repos().Create(secretpath.Join(repoPath))
-	if err != nil {
-		cmd.progressPrinter.Stop()
-		return err
-	}
-
-	secretPath := secretpath.Join(repoPath, "hello")
-	message := fmt.Sprintf("Welcome %s! This is your first secret. To write a new version of this secret, run:\n\n    secrethub write %s", cmd.fullName, secretPath)
-
-	_, err = client.Secrets().Write(secretPath, []byte(message))
+	secretPath, err := createStartRepo(client, cmd.username, cmd.fullName)
 	if err != nil {
 		cmd.progressPrinter.Stop()
 		return err
@@ -188,44 +177,75 @@ func (cmd *SignUpCommand) Run() error {
 	cmd.progressPrinter.Stop()
 	fmt.Fprint(cmd.io.Output(), "Created your account.\n\n")
 
-	createWorkspace := cmd.org != ""
-	if !createWorkspace {
-		createWorkspace, err = ui.AskYesNo(cmd.io, "Do you want to create a shared workspace for your team?", ui.DefaultYes)
+	err = createWorkspace(client, cmd.io, cmd.org, cmd.orgDescription, cmd.progressPrinter)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(cmd.io.Output(), "Setup complete. To read your first secret, run:\n\n    secrethub read %s\n\n", secretPath)
+
+	return nil
+}
+
+// createStartRepo creates a start repository and writes a fist secret to it, so that
+// the user can start by reading their first secret. It returns the secret's path.
+// This is intended to smoothen onboarding.
+func createStartRepo(client secrethub.ClientInterface, username string, fullName string) (string, error) {
+	repoPath := secretpath.Join(username, "start")
+	_, err := client.Repos().Create(secretpath.Join(repoPath))
+	if err != nil {
+		return "", err
+	}
+
+	secretPath := secretpath.Join(repoPath, "hello")
+	message := fmt.Sprintf("Welcome %s! This is your first secret. To write a new version of this secret, run:\n\n    secrethub write %s", fullName, secretPath)
+
+	_, err = client.Secrets().Write(secretPath, []byte(message))
+	if err != nil {
+		return "", err
+	}
+	return secretPath, nil
+}
+
+// createWorkspace c
+func createWorkspace(client secrethub.ClientInterface, io ui.IO, org string, orgDescription string, progressPrinter progress.Printer) error {
+	if org == "" {
+		createWorkspace, err := ui.AskYesNo(io, "Do you want to create a shared workspace for your team?", ui.DefaultYes)
 		if err != nil {
 			return err
 		}
-		fmt.Fprintln(cmd.io.Output())
+		fmt.Fprintln(io.Output())
 		if !createWorkspace {
-			fmt.Fprint(cmd.io.Output(), "You can create a shared workspace later using `secrethub org init`.\n\n")
+			fmt.Fprint(io.Output(), "You can create a shared workspace later using `secrethub org init`.\n\n")
+			return nil
 		}
 	}
-	if createWorkspace {
-		if cmd.org == "" {
-			cmd.org, err = ui.AskAndValidate(cmd.io, "Workspace name (e.g. your company name): ", 2, api.ValidateOrgName)
-			if err != nil {
-				return err
-			}
-		}
-		if cmd.orgDescription == "" {
-			cmd.orgDescription, err = ui.AskAndValidate(cmd.io, "A description (max 144 chars) for your team workspace so others will recognize it:\n", 2, api.ValidateOrgDescription)
-			if err != nil {
-				return err
-			}
-		}
-		fmt.Fprint(cmd.io.Output(), "Creating your shared workspace...")
-		cmd.progressPrinter.Start()
 
-		_, err := client.Orgs().Create(cmd.org, cmd.orgDescription)
-		cmd.progressPrinter.Stop()
-		if err == api.ErrOrgAlreadyExists {
-			fmt.Fprintf(cmd.io.Output(), "The workspace %s already exists. If it is your organization, ask a colleague to invite you to the workspace. You can also create a new one using `secrethub org init`.\n", cmd.org)
-		} else if err != nil {
+	var err error
+	if org == "" {
+		org, err = ui.AskAndValidate(io, "Workspace name (e.g. your company name): ", 2, api.ValidateOrgName)
+		if err != nil {
 			return err
-		} else {
-			fmt.Fprint(cmd.io.Output(), "Created your shared workspace.\n\n")
 		}
 	}
-	fmt.Fprintf(cmd.io.Output(), "Setup complete. To read your first secret, run:\n\n    secrethub read %s\n\n", secretPath)
+	if orgDescription == "" {
+		orgDescription, err = ui.AskAndValidate(io, "A description (max 144 chars) for your team workspace so others will recognize it:\n", 2, api.ValidateOrgDescription)
+		if err != nil {
+			return err
+		}
+	}
 
+	fmt.Fprint(io.Output(), "Creating your shared workspace...")
+	progressPrinter.Start()
+
+	_, err = client.Orgs().Create(org, orgDescription)
+	progressPrinter.Stop()
+	if err == api.ErrOrgAlreadyExists {
+		fmt.Fprintf(io.Output(), "The workspace %s already exists. If it is your organization, ask a colleague to invite you to the workspace. You can also create a new one using `secrethub org init`.\n", org)
+	} else if err != nil {
+		return err
+	} else {
+		fmt.Fprint(io.Output(), "Created your shared workspace.\n\n")
+	}
 	return nil
 }

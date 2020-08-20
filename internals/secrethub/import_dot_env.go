76b736d37c5d8a019d65d8b764a1858600d536c1
Horia Culea <horiaculea@gmail.com>
2020-08-20 17:06:41 +0300

    working interactive flag

--- internals/secrethub/import_dot_env.go
+++ internals/secrethub/import_dot_env.go
@@ -2,6 +2,7 @@ package secrethub
 
 import (
 	"fmt"
+
 	"github.com/joho/godotenv"
 	"github.com/secrethub/secrethub-cli/internals/cli/ui"
 	"github.com/secrethub/secrethub-cli/internals/secrethub/command"
@@ -14,6 +15,7 @@ type ImportDotEnvCommand struct {
 	path        api.DirPath
 	interactive bool
 	dotenv      string
+	editor      string
 	newClient   newClientFunc
 }
 
@@ -29,9 +31,9 @@ func NewImportDotEnvCommand(io ui.IO, newClient newClientFunc) *ImportDotEnvComm
 func (cmd *ImportDotEnvCommand) Register(r command.Registerer) {
 	clause := r.Command("dot-env", "Import secrets from .env files.")
 	clause.Arg("dir-path", "The path to where to write the new secrets").PlaceHolder(dirPathPlaceHolder).SetValue(&cmd.path)
-	clause.Flag("interactive", "Edit the paths to where the secrets should be written.").Short('i').BoolVar(&cmd.interactive)
+	clause.Flag("interactive", "Interactive mode. Edit the paths to where the secrets should be written.").Short('i').BoolVar(&cmd.interactive)
 	clause.Flag("env-file", "The location of the .env file. Defaults to `.env`.").Default(".env").ExistingFileVar(&cmd.dotenv)
-
+	clause.Flag("editor", "The editor where you will define your secret paths. Only available in the interactive mode.").Default("nano").HintOptions("vim", "nano").StringVar(&cmd.editor)
 	command.BindAction(clause, cmd.Run)
 }
 
@@ -47,17 +49,38 @@ func (cmd *ImportDotEnvCommand) Run() error {
 		return err
 	}
 
-	for key, value := range envVar {
-		_, err := client.Secrets().Write(cmd.path.Value()+"/"+key, []byte(value))
+	if cmd.interactive {
+		locationsMap, err := openEditor(cmd.editor, cmd.path.Value(), getMapKeys(envVar))
 		if err != nil {
 			return err
 		}
-	}
 
+		for key, value := range envVar {
+			_, err := client.Secrets().Write(locationsMap[key], []byte(value))
+			if err != nil {
+				return err
+			}
+		}
+	} else {
+		for key, value := range envVar {
+			_, err := client.Secrets().Write(cmd.path.Value()+"/"+key, []byte(value))
+			if err != nil {
+				return err
+			}
+		}
+	}
 	_, err = fmt.Fprintf(cmd.io.Output(), "Transfer complete! The secrets have been written to %s.\n", cmd.path.String())
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func getMapKeys(stringMap map[string]string) []string {
+	keys := make([]string, 0, len(stringMap))
+	for k := range stringMap {
+		keys = append(keys, k)
+	}
+	return keys
 }

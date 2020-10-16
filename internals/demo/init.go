fd69cd23249fb84fbb1d231499531ff4d1266240
Horia Culea <horiaculea@gmail.com>
2020-10-16 12:56:46 +0300

    Fix demo init, update BindArguments parameters

--- internals/demo/init.go
+++ internals/demo/init.go
@@ -39,7 +39,7 @@ func (cmd *InitCommand) Register(r cli.Registerer) {
 	clause.Flags().VarPF(&cmd.repo, "repo", "", "The path of the repository to create. Defaults to a "+defaultDemoRepo+" repo in your personal namespace.")
 
 	clause.BindAction(cmd.Run)
-	clause.BindArguments(nil, nil)
+	clause.BindArguments(nil)
 }
 
 // Run handles the command with the options as specified in the command.

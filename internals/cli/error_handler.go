75f7fc22e105c8b814f0cd5bfed878ab358366c6
Horia Culea <horiaculea@gmail.com>
2020-10-19 14:18:44 +0300

    Modularize argument error handling

--- internals/cli/error_handler.go
+++ internals/cli/error_handler.go
@@ -13,13 +13,20 @@ func (c *CommandClause) argumentError(args []string) error {
 	} else {
 		errorText += fmt.Sprintf("`secrethub "+c.fullCommand()+"` requires between %d and %d arguments.", minimum, maximum)
 	}
-	errorText += "\n\nSee `secrethub " + c.fullCommand() + " --help` for help.\n\n"
-	errorText += "Usage: " + "secrethub " + c.fullCommand() + " [FLAGS] "
-
-	for _, args := range c.Args {
-		errorText += "<" + args.Name + "> "
-	}
+	errorText += "\n\nSee `secrethub " + c.fullCommand() + " --help` for help.\n\n" + c.usage()
 	errorText += "\n\n" + c.Cmd.Short
 
 	return fmt.Errorf(errorText)
+}
+
+func (c *CommandClause) usage() string {
+	usage := "Usage: " + "secrethub " + c.fullCommand() + " [FLAGS] "
+	for _, args := range c.Args {
+		usage += "<" + args.Name + "> "
+	}
+	return usage
+}
+
+func (c *CommandClause) Help() string {
+	return ""
 }

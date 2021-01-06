3ac41e753372d557c9df9b2dada1b64423198f05
Eduard Filip <eduard.filip@secrethub.io>
2021-01-06 19:49:28 +0200

    Make cleaner error messages.

--- internals/cli/error_handler.go
+++ internals/cli/error_handler.go
@@ -9,12 +9,19 @@ func (c *CommandClause) argumentError(args []string) error {
 	errorText, minimum, maximum := "", getRequired(c.Args), len(c.Args)
 
 	if minimum == maximum {
-		errorText += fmt.Sprintf("`"+c.fullCommand()+"` requires exactly %d argument(s).", minimum)
+		errorText += fmt.Sprintf(`"%s" requires exactly %d %s.`, c.fullCommand(), minimum, pluralize("argument", minimum))
 	} else {
-		errorText += fmt.Sprintf("`"+c.fullCommand()+"` requires between %d and %d arguments.", minimum, maximum)
+		errorText += fmt.Sprintf(`"%s" requires between %d and %d arguments.`, c.fullCommand(), minimum, maximum)
 	}
-	errorText += "\n\nSee " + c.fullCommand() + " --help` for help.\n\n" + useLine(c.Cmd, c.Args)
-	errorText += "\n\n" + c.Cmd.Short
+
+	errorText += fmt.Sprintf("\nSee `%s --help` for help.\n\nUsage: %s\n\n%s", c.fullCommand(), useLine(c.Cmd, c.Args), c.Cmd.Short)
 
 	return fmt.Errorf(errorText)
+}
+
+func pluralize(word string, num int) string {
+	if num == 1 {
+		return word
+	}
+	return word + "s"
 }

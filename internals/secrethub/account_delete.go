896dba80406e734494b485e5628d8e9671983e4a
Marton S <marton.soos@secrethub.io>
2021-03-31 15:32:07 +0300

    Add client side check for org membership to account delete

--- internals/secrethub/account_delete.go
+++ internals/secrethub/account_delete.go
@@ -1,9 +1,13 @@
 package secrethub
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/secrethub/secrethub-cli/internals/cli"
 	"github.com/secrethub/secrethub-cli/internals/cli/ui"
 	"github.com/secrethub/secrethub-go/internals/api"
+	"github.com/secrethub/secrethub-go/pkg/secrethub/iterator"
 )
 
 // AccountDeleteCommand is a command to inspect account details.
@@ -38,5 +42,29 @@ func (cmd *AccountDeleteCommand) Run() error {
 	if err != nil {
 		return err
 	}
+
+	var orgList []api.Org
+	orgIterator := client.Orgs().Iterator(nil)
+	for {
+		var org api.Org
+		org, err = orgIterator.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return err
+		}
+		orgList = append(orgList, org)
+	}
+	if len(orgList) > 0 {
+		builder := strings.Builder{}
+		for i, org := range orgList {
+			builder.WriteString(org.Name)
+			if i != len(orgList)-1 {
+				builder.WriteString(", ")
+			}
+		}
+		return errors.New("cannot delete account that is a member of an organization. Please leave or delete the following organizations before deleting your account: " + builder.String() + ".")
+	}
+
 	return client.Accounts().Delete(account.AccountID)
 }

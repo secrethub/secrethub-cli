c450c9929223aadf0b00c39c5d1aea6fc82d5e64
Marton Soos <marton.soos@secrethub.io>
2021-04-30 21:08:22 +0300

    Run goimports

--- internals/secrethub/account_delete.go
+++ internals/secrethub/account_delete.go
@@ -2,11 +2,12 @@ package secrethub
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/secrethub/secrethub-cli/internals/cli"
 	"github.com/secrethub/secrethub-cli/internals/cli/ui"
 	"github.com/secrethub/secrethub-go/internals/api"
 	"github.com/secrethub/secrethub-go/pkg/secrethub"
-	"os"
 )
 
 // AccountDeleteCommand is a command to inspect account details.
